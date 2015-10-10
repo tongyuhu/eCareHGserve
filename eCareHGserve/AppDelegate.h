246be8f26143932cddaae60cc2f5b0d833575e1b
tongyuhu <tongyuhu@tongyuhudiannaoWiFi.local>
2015-10-10 10:09:42 +0800

    新建工程
    新建工程

---
+++ eCareHGserve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eCareHGserve
+//
+//  Created by tongyuhu on 15/10/10.
+//  Copyright (c) 2015年 SHmoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eCareHGserve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eCareHGserve
+//
+//  Created by tongyuhu on 15/10/10.
+//  Copyright (c) 2015年 SHmoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
